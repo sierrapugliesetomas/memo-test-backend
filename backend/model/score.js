cd5b4693afe2ede389750ba8c2a2343de73bdc39
Tomas Sierra Pugliese <sierrapugliesetomas@gmail.com>
2023-09-22 15:17:04 -0300

    Se añaden dificultades

--- backend/model/score.js
+++ backend/model/score.js
@@ -4,6 +4,7 @@ const scoreSchema = mongoose.Schema({
   playerName: { type: String, required: true },
   time: { type: Number, required: true },
   moves: { type: Number, required: false },
+  difficulty: { type: String, required: true }
 });
 
 module.exports = mongoose.model("Score", scoreSchema);
--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -1,45 +1,17 @@
 const express = require("express");
-// const multer = require("multer");
 
 const Score = require("../model/score");
 
 const router = express.Router();
 
-// const MIME_TYPE_MAP = {
-//   "image/png": "png",
-//   "image/jpeg": "jpg",
-//   "image/jpg": "jpg"
-// };
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     const isValid = MIME_TYPE_MAP[file.mimetype];
-//     let error = new Error("Invalid mime type");
-//     if (isValid) {
-//       error = null;
-//     }
-//     cb(error, "backend/images");
-//   },
-//   filename: (req, file, cb) => {
-//     const name = file.originalname
-//       .toLowerCase()
-//       .split(" ")
-//       .join("-");
-//     const ext = MIME_TYPE_MAP[file.mimetype];
-//     cb(null, name + "-" + Date.now() + "." + ext);
-//   }
-// });
-
 router.post(
   "",
-  //   multer({ storage: storage }).single("image"),
   (req, res, next) => {
-    // const url = req.protocol + "://" + req.get("host");
     const score = new Score({
       playerName: req.body.playerName,
       time: req.body.time,
       moves: req.body.moves,
-      //   imagePath: url + "/images/" + req.file.filename
+      difficulty: req.body.difficulty
     });
     score.save().then((createdScore) => {
       res.status(201).json({
@@ -53,9 +25,8 @@ router.post(
   }
 );
 
-
-router.get("", (req, res, next) => {
-  Score.find().sort({time: 'asc'}).limit(10).then((documents) => {
+router.get("/", (req, res, next) => {
+  Score.find({ difficulty: { $eq: req.query.difficulty}}).sort({time: 'asc'}).limit(10).then((documents) => {
     res.status(200).json({
       message: "Scores fetched successfully!",
       scores: documents,
@@ -68,7 +39,7 @@ router.get("/:id", (req, res, next) => {
     if (post) {
       res.status(200).json(post);
     } else {
-      res.status(404).json({ message: "Post not found!" });
+      res.status(404).json({ message: "Score not found!" });
     }
   });
 });
